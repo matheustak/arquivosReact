603e256ec08b539701bd1f5e61ee939b8211dabb
matheus <matheusrochaalecrim@gmail.com>
2023-11-15 17:39:40 -0400

    update events.js
    ccomo não se implementar um button evento

--- 2_FUNDAMENTOS/src/components/Events.js
+++ 2_FUNDAMENTOS/src/components/Events.js
@@ -9,6 +9,22 @@ return (
     <div>
         <div>
             <button onClick={handleMyEvent}>Clique aqui!</button>
+            
+        </div>
+        <div>
+            <button onClick={() => console.log("Clicou!")}>
+                Clique aqui também!
+            </button>
+
+            <button
+            onClick={() => {
+            if (true){
+            console.log("Isso não deveria existir =)");
+            }
+        }}
+            >
+                Clica aqui, por favor.
+            </button>
         </div>
     </div>
 );
