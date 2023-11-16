ec801a4277df6a969b0fa93079a165f596ab5480
matheus <matheusrochaalecrim@gmail.com>
2023-11-15 20:40:24 -0400

    renderização
    renderização fora do return

--- 2_FUNDAMENTOS/src/components/Events.js
+++ 2_FUNDAMENTOS/src/components/Events.js
@@ -4,6 +4,15 @@ const Events = () => {
         console.log("Ativou o evento!");
     };
 
+    const renderSomething =(x) =>   {
+        if (x) {
+           return <h1>Renderizando isso!</h1>;
+
+        }else{
+            return <h1>Também posso renderizar isso!</h1>;
+        }
+    }
+
 
 return (
     <div>
@@ -26,6 +35,9 @@ return (
                 Clica aqui, por favor.
             </button>
         </div>
+
+       {renderSomething(true)}
+       {renderSomething(false)} 
     </div>
 );
 
