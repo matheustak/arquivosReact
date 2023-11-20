056ed7872e1f6b3e221ff2e95daab15215c2405b
matheus <matheusrochaalecrim@gmail.com>
2023-11-20 15:55:57 -0400

    CSS Inline
    CSS Inline dinâmico

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -2,8 +2,11 @@
 //Componentes
 import './App.css';
 import MyComponent from './components/MyComponent';
+import {useState} from "react";
 
 function App() {
+  const n = 15;
+  const [name] = useState("Matheus");
   return (
     <div className="App">
       {/* CSS GLOBAL */}
@@ -12,8 +15,41 @@ function App() {
      <MyComponent />
      <p>Este parágrafo é do App.js</p>
      {/* Inline CSS */}
-     <p style={{color: "blue", padding: "25px", borderTop:"2px solid red"}}>Este elemento foi estilizado de forma inline</p>
+     <p style={{
+      color: "blue",
+       padding: "25px",
+        borderTop:"2px solid red",
+        }}
+        >
+          Este elemento foi estilizado de forma inline
+          </p>
+          <p
+           style={{
+      color: "magenta",
+       padding: "25px",
+        borderTop:"2px solid red",
+        }}
+        >
+          Este elemento foi estilizado de forma inline
+          </p>
     
+    {/* CSS Inline dinâmico */}
+    <h2 style={n < 10? { color:"purple"} : { color: "pink"}}> 
+      CSS dinâmico
+    </h2>
+    <h2 style={n > 10? { color:"purple"} : { color: "pink"}}> 
+      CSS dinâmico
+    </h2>
+
+    <h2
+     style={
+      name === "Matheus" 
+      ? { color:"green",  backgroundColor: "#000"}
+      :null
+     }
+     > 
+     Teste nome
+    </h2>
 
     </div>
   );
