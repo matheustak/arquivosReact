ea4ed2472efb2f9c7766255481563dd007709fb7
matheus <matheusrochaalecrim@gmail.com>
2023-11-20 13:00:30 -0400

    CSS
    treinando Css como não vaza o css do componente

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,10 +1,18 @@
 
+//Componentes
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS GLOBAL */}
      <h1>React com CSS</h1>
+     {/* CSS de Componente */}
+     <MyComponent />
+     <p>Este parágrafo é do App.js</p>
+     <p className="my-comp-paragraph">Este tb é do componente</p>
+
     </div>
   );
 }
---
+++ 4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import './MyComponent.css';
+
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>CSS de Componente</h1>
+        <p>Este é o paragrafo do componente!</p>
+        </div>
+  );
+};
+
+export default MyComponent;
