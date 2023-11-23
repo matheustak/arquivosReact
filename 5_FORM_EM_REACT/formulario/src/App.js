0f6c6d1c9ba7ee41f17210ce21bbdda3af5db2d5
matheus <matheusrochaalecrim@gmail.com>
2023-11-23 19:02:50 -0400

    formularios
    formularios com css html js react

--- 5_FORM_EM_REACT/formulario/src/App.js
+++ 5_FORM_EM_REACT/formulario/src/App.js
@@ -1,12 +1,17 @@
 
+
+//components
+import MyForm from './components/MyForm';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h2>Forms</h2>
+     <MyForm />
     </div>
   );
-}
+};
 
 export default App;
---
+++ 5_FORM_EM_REACT/formulario/src/components/MyForm.js
@@ -0,0 +1,21 @@
+import './Myform.css'
+
+const MyForm = () => {
+  return (
+
+    <div>
+    {/* 1 - criacao de form */}
+    <form>
+        <div>
+            <label htmlFor="name">Nome:</label>
+            <input type="text" name="name" placeholder="Digite o seu nome" />
+        </div>
+
+        <input type="submit"  value="Enviar"/> 
+    </form>
+
+    </div>
+  )
+}
+
+export default MyForm
