8f312cbd3a3d08c27ed91da949c2ab6b54b7c77a
matheus <matheusrochaalecrim@gmail.com>
2023-11-23 19:34:47 -0400

    Update myform
    gerenciando dados com função usando hooks useState e inline

--- 5_FORM_EM_REACT/formulario/src/components/MyForm.js
+++ 5_FORM_EM_REACT/formulario/src/components/MyForm.js
@@ -1,6 +1,20 @@
 import './Myform.css'
 
+import {useState} from 'react'
+
 const MyForm = () => {
+
+// 3- gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    };
+
+    console.log(name);
+    console.log(email);
+
   return (
 
     <div>
@@ -8,12 +22,15 @@ const MyForm = () => {
     <form>
         <div>
             <label htmlFor="name">Nome:</label>
-            <input type="text" name="name" placeholder="Digite o seu nome" />
+            <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName}/>
         </div>
         {/* 2 - Label envolvendo input */}
         <label>
             <span>E-mail</span>
-            <input type="email" name="email" placeholder="Digite o seu e-mail" />
+            <input type="email"
+             name="email" 
+             placeholder="Digite o seu e-mail"
+            onChange ={(e) => setEmail(e.target.value)} />
         </label>
 
         <input type="submit"  value="Enviar"/> 
