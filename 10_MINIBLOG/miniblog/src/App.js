d0004d92243c34bf90aa8314c64134d1108dfd3f
matheus <matheusrochaalecrim@gmail.com>
2024-01-09 19:17:36 -0400

    Login
    autenticando usuario criando conta no firebase

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 import {BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from 'firebase/auth';
+
+//hooks
+import { useState, useEffect} from "react";
+import { useAuthentication } from './hooks/useAuthentication';
+
+//Context
+import { AuthProvider } from './context/AuthContext';
 
 //Pages
 import Home from  "./pages/Home/Home";
 import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 //components
 import Navbar from "./components/Navbar";
@@ -12,18 +22,39 @@ import Footer from "./components/Footer";
 
 
 function App() {
+
+  const [user, setUser] = useState(undefine);
+  const {auth} = useAuthentication();
+
+  const loadingUser = user === undefined;
+useEffect(() =>{
+  onAuthStateChanged(auth,(user) => {
+    setUser(user);
+  });
+},[auth]);
+
+
+  if (loadingUser){
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="App">
+      <AuthProvider value={{user}}>
       <BrowserRouter>
       <Navbar />
         <div className='container'>
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+
           </Routes>
         </div>
         <Footer />
       </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
